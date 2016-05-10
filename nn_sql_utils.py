f83acb58fb7cdd7dc45046c8df7efdda19e08725
ags <arturgs@gmail.com>
2016-05-10 12:33:20 +0200

    sql changes

--- nn_sql_utils.py
+++ nn_sql_utils.py
@@ -2,14 +2,11 @@ import mysql.connector
 from datetime import date
 
 
-
 # we store a song for each user creation OR neural network output
 def sql_record_song(name, when, creator, generation, score):
-	cnx = mysql.connector.connect(user='root', password='',
-                              host='127.0.0.1',
-                              database='snr16_test')
-
+	cnx = sql_connect()
 	cursor = cnx.cursor()
+
 	add_song = ("INSERT INTO songs "
                "(name, time, creator, generation, score) "
                "VALUES (%s, %s, %s, %s, %s)")
@@ -24,11 +21,9 @@ def sql_record_song(name, when, creator, generation, score):
 
 # only users score a song HIT<=>SHIT
 def sql_record_score(when, song_id, score, user):
-	cnx = mysql.connector.connect(user='root', password='',
-                              host='127.0.0.1',
-                              database='snr16_test')
-
+	cnx = sql_connect()
 	cursor = cnx.cursor()
+
 	add_score = ("INSERT INTO songs "
                "(time, song, score, user) "
                "VALUES (%s, %s, %s, %s)")
@@ -42,11 +37,9 @@ def sql_record_score(when, song_id, score, user):
 
 # we store information about each NN after training
 def sql_record_nn(network, when, file_hd5):
-	cnx = mysql.connector.connect(user='root', password='',
-                              host='127.0.0.1',
-                              database='snr16_test')
-
+	cnx = sql_connect()
 	cursor = cnx.cursor()
+
 	add_nn = ("INSERT INTO nn "
                "(name, when, file_hd5) "
                "VALUES (%s, %s, %s)")
@@ -60,14 +53,13 @@ def sql_record_nn(network, when, file_hd5):
 
 # for each instance of the NN, we keep the songs on the training set
 def sql_record_training(network, songs):	
-	cnx = mysql.connector.connect(user='root', password='',
-                              host='127.0.0.1',
-                              database='snr16_test')
-	
+	cnx = sql_connect()
 	cursor = cnx.cursor()
+
 	add_training = ("INSERT INTO nn "
                "(network, song) "
                "VALUES (%s, %s)")
+
 	for song in songs:
 		cursor.execute(add_training, (network, song))
 
@@ -76,6 +68,12 @@ def sql_record_training(network, songs):
 	cnx.close()
 	return
 
+
+def sql_connect():
+	cnx = mysql.connector.connect(user='root', password='',
+        		host='127.0.0.1',
+        		database='snr16_test')
+	return cnx
 
 
 sql_record_song('df2',date(2016,05,02),001,0,0)
