bfc4448fc9ec0d961ea1226d4bb85f9f38b3dfd6
ags <arturgs@gmail.com>
2016-05-11 10:41:46 +0200

    tests added

--- nn_server.py
+++ nn_server.py
@@ -1,9 +1,14 @@
+import nn_utils as nnu
+import nn_sql_utils as sqlu
+import nn_soundcloud_utils as scu
+
+import os
 
 
 # store the score given by a user to song
 def set_Score(song, score):
-	# check song in DB
 	# store new score
+	
 	
 
 
@@ -18,25 +23,37 @@ def store_song(song, user):
 
 # returns a list of wav songs to evaluate by users
 def get_song_collection():
-	# ? DO WE TRUST TO RANDOM PICKING HERE
+	for songs in os.listdir(MUSIC_DIRECTORY):
+		
+		
 
 
 
 # publish media on SN
 def publish():
-	# upload to soundcloud
-	# tweet new song
+	# upload WAV to soundcloud & tweet new song
+	scu.upload_soundcloud(title, song_path, token)
 
 
 
 # train again the network with a new training set
 def batch_work():
 	# get_song_collection from natural selection
+	training_set = get_song_collection()
 	# train
+	network = nnu.train_network(training_set)
 	# store training set information	
+	sqlu.sql_record_training(network, training_set)
 	# store network information
-	# generate new collection
+	when = date()
+	sqlu.sql_record_nn(network, when, file_hd5)
+	# generate new collection & store
+	new_collection = nnu.generate_media()
+	for song in new_collection:
+		store_song(song, user)
 	# publish new media
+	publish()
+	
 
 
 
---
+++ tests/client.py
@@ -0,0 +1,27 @@
+
+
+
+
+def test_suite_HS():
+	
+	# get song collection
+
+	# loop over collection, collect votes and store them
+
+
+def test_suite_compose():
+
+	# generate_songs
+
+	# store songs in folder
+
+	# store song's information
+
+
+def test_suite_visualization():
+
+
+
+
+
+
